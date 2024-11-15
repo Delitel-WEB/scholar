158fab5e6d4080ce5b5053f753a9cc37cb1c7f77
Delitel <delitel1@mail.ru>
2024-11-15 13:32:51 +0300

    feat: 3 попытки генерации рекомендаций

--- bot/utils/recomendations.py
+++ bot/utils/recomendations.py
@@ -1,7 +1,7 @@
 from ..core import client
 
 
-async def generate_recommendation(topic, references) -> str:  # noqa: ANN001
+async def generate_recommendation(topic, references, attempts=3) -> str:  # noqa: ANN001
     """
     Генерирует рекомендации для курсовой работы на основе полученных источников.
 
@@ -9,12 +9,20 @@ async def generate_recommendation(topic, references) -> str:  # noqa: ANN001
     :param references: Список источников.
     :return: Рекомендация для курсовой работы.
     """
+
+    if attempts <= 0:
+        return "Не удалось подготовить рекомендации!"
+    
     sources_text = "\n".join([f"{i+1}. {ref['title']} - {ref['authors']} ({ref['year']})"for i, ref in enumerate(references)])
     prompt = f"Тема курсовой работы: {topic}\nИсточники:\n{sources_text}\n\nСформулируй рекомендации для написания\n" \
              "курсовой работы, основываясь на этих источниках. Напиши ТОЛЬКО рекомендации! НА РУССКОМ ЯЗЫКЕ!"
 
     recommendation = await client.chat.completions.async_create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": prompt}],
-)
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}],
+    )
+    if recommendation.choices[0].message.content == "Request ended with status code 404":
+        return await generate_recommendation(topic, references, attempts-1)
+
+
     return recommendation.choices[0].message.content
